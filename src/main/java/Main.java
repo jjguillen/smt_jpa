302128e5a54ca07f5c03562bb459b9b2ace370d0
jjguillen <jjavierguillen@gmail.com>
2026-04-28 17:59:08 +0200

    Main añadiendo dos viajeros

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,12 +15,18 @@ public class Main {
         IO.println("Arrancando app Servicio Municipal de Transportes");
 
         ViajeroRepository viajeroRepo = new ViajeroRepository();
+
+        /*
         viajeroRepo.save(new Viajero(null, "58589874A","Manolo Lama",
                 56, "Antas", TipoAbono.ANUAL, 150, null));
         viajeroRepo.save(new Viajero(null, "65432147B","Marta Prendes",
                 38, "Vera", TipoAbono.MENSUAL, 110, null));
 
 
+        Viajero v1 = viajeroRepo.findById(1L).orElse(null);
+        v1.setSaldoPuntos(200);
+        viajeroRepo.update(v1);
+         */
 
 
 
--- src/main/java/repositories/ViajeRepository.java
+++ src/main/java/repositories/ViajeRepository.java
@@ -3,7 +3,10 @@ package repositories;
 import entities.Viaje;
 
 public class ViajeRepository extends JpaRepository<Viaje, Long> {
+
     public ViajeRepository() {
         super(Viaje.class);
     }
+
+
 }
